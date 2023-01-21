529300f32d917a6d802eed348b37dfb294fd76ba
yjyaang <topbuty97@gmail.com>
2023-01-22 01:16:20 +0900

    fix: image optimization

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,66 +1,66 @@
 export const images = [
     {
         name: '오늘의 유영혁',
-        src: 'evolution.png'
+        src: 'evolution.jpg'
     },
     {
         name: '요술공주 윾키',
-        src: 'princess.png'
+        src: 'princess.jpg'
     },
     {
         name: '가오링스타',
-        src: 'countingstar.png'
+        src: 'countingstar.jpg'
     },
     {
         name: '유영혁 - 내사람',
-        src: 'mydear.png'
+        src: 'mydear.jpg'
     },
     {
         name: '김치섞은밥 요리사',
-        src: 'kimchifriedrice.png'
+        src: 'kimchifriedrice.jpg'
     },
     {
         name: '목 긋는 주장',
-        src: 'youdie.png'
+        src: 'youdie.jpg'
     },
     {
         name: '어린이 영혁',
-        src: 'children.png'
+        src: 'children.jpg'
     },
     {
         name: '주먹팡팡',
-        src: 'punch.png'
+        src: 'punch.jpg'
     },
     {
         name: '오리..아니 거위 영혁',
-        src: 'goose.png'
+        src: 'goose.jpg'
     },
     {
         name: '하트영혁',
-        src: 'heart.png'
+        src: 'heart.jpg'
     },
     {
         name: '다 방송 꺼',
-        src: 'turnoff.png'
+        src: 'turnoff.jpg'
     },
     {
         name: '끄덕끄덕 주장',
-        src: 'nodding.png'
+        src: 'nodding.jpg'
     },
     {
         name: '유영혁 - 서울의 달',
-        src: 'seoulmoon.png'
+        src: 'seoulmoon.jpg'
     },
     {
         name: '유영혁 - 잘할걸',
-        src: 'jhg.png'
+        src: 'jhg.jpg'
     },
     {
         name: '어어 너도 카트하니?',
-        src: 'kartinthecastle.png'
+        src: 'kartinthecastle.jpg'
     },
     {
         name: '유버스 타고 우~',
-        src: 'ubuswoo.png'
+        src: 'ubuswoo.jpg'
     },
 ]
